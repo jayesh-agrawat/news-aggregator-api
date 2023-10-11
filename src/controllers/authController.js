dba3730eba0bdd513b328559481c664293d21b3a
Jayesh <jayesh.work@hotmail.com>
2023-10-11 19:58:01 +0530

    removed commented code in UserAuth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,7 @@
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcrypt");
 var cache = require("../data/cache");
-// var UserSchema = require("../models/user");
-const userSchema = require("../models/user");
-const Joi = require("joi");
+var userSchema = require("../models/user");
 
 var signup = (req, res) => {
   const userData = {
@@ -24,18 +22,6 @@ var signup = (req, res) => {
       message: "User Saved Succesfully",
     });
   }
-  // user
-  //   .save()
-  //   .then((data) => {
-  //     return res.status(200).send({
-  //       message: "User Saved Succesfully",
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     return res.status(500).send({
-  //       message: err,
-  //     });
-  //   });
 };
 
 var signin = (req, res) => {
@@ -71,29 +57,6 @@ var signin = (req, res) => {
   } else {
     return res.status(404).send({ message: "User not found" });
   }
-
-  // User.findOne({
-  //   email: emailPassed,
-  // }).then((user) => {
-  //   if (!user) return res.status(404).send({ message: "User not found" });
-  //   var passwordIsValid = bcrypt.compareSync(passwordPassed, user.password);
-  //   if (!passwordIsValid) {
-  //     return res.status(401).send({
-  //       message: "Invalid Password",
-  //     });
-  //   }
-
-  var token = jwt.sign(
-    {
-      id: user.id,
-    },
-    process.env.API_SECRET,
-    {
-      expiresIn: 86400,
-    }
-  );
-
-  // });
 };
 
 module.exports = {
